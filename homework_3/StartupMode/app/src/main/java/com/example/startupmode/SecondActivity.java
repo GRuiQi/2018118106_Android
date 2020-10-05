c4ae1851e4c3b9a2db1b8a10e14ab1e0d4b4110d
GRuiQi <48426540+GRuiQi@users.noreply.github.com>
2020-10-05 12:21:32 +0800

    完成singleInstance模式

--- homework_3/StartupMode/app/src/main/java/com/example/startupmode/SecondActivity.java
+++ homework_3/StartupMode/app/src/main/java/com/example/startupmode/SecondActivity.java
@@ -14,23 +14,26 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        Log.d("SecondActivity",this.toString());
+        //Log.d("SecondActivity",this.toString());
+        Log.d("SecondActivity","Tast id is"+getTaskId());
         Button button2 = (Button)findViewById(R.id.button2);
         button2.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                /* Intent intent = new Intent(FirstActivity.this,
                         FirstActivity.class);*/
+                /*Intent intent = new Intent(SecondActivity.this,
+                        FirstActivity.class);*/
                 Intent intent = new Intent(SecondActivity.this,
-                        FirstActivity.class);
+                        ThirdActivity.class);
                 startActivity(intent);
             }
         });
     }
 
-    @Override
+   /* @Override
     protected void onDestroy(){
         super.onDestroy();
         Log.d("SecondActivity","onDestroy");
-    }
+    }*/
 }
