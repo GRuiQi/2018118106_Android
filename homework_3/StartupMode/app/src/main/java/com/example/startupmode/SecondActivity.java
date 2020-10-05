2a8de2693f77bcecd9218d4b2941d3dffe0db424
GRuiQi <48426540+GRuiQi@users.noreply.github.com>
2020-10-05 11:22:08 +0800

    完成singleTop模式

---
+++ homework_3/StartupMode/app/src/main/java/com/example/startupmode/SecondActivity.java
@@ -0,0 +1,30 @@
+package com.example.startupmode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Log.d("SecondActivity",this.toString());
+        Button button2 = (Button)findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+               /* Intent intent = new Intent(FirstActivity.this,
+                        FirstActivity.class);*/
+                Intent intent = new Intent(SecondActivity.this,
+                        FirstActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
