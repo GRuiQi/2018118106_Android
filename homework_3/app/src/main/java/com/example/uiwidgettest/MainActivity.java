7492ecfaa33f773cacc40fb23fef7ec3aba3b7be
GRuiQi <2654344573@qq.com>
2020-10-18 22:38:48 +0800

    完成动态更改ImageView中的图片

--- homework_3/app/src/main/java/com/example/uiwidgettest/MainActivity.java
+++ homework_3/app/src/main/java/com/example/uiwidgettest/MainActivity.java
@@ -2,15 +2,18 @@ package com.example.uiwidgettest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editText;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +21,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.button);
         editText = (EditText)findViewById(R.id.edit_text);
+        imageView = (ImageView)findViewById(R.id.image_view);
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 //System.out.println("注册监听器");
                 switch (v.getId()) {
                     case R.id.button:
-                        String inputText = editText.getText().toString();
+                       /* String inputText = editText.getText().toString();
                         Toast.makeText(MainActivity.this,inputText,
-                                Toast.LENGTH_LONG).show();
+                                Toast.LENGTH_LONG).show();*/
+
+                       imageView.setImageResource(R.drawable.img_2);
+
                         break;
                     default:
                         break;
