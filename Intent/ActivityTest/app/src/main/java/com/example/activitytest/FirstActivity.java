1bf09ce6d92a725bcc5b743b153fdc9c0ca78e10
GRuiQi <2654344573@qq.com>
2020-11-17 00:31:31 +0800

    隐式Intent调用系统拨号界面

--- Intent/ActivityTest/app/src/main/java/com/example/activitytest/FirstActivity.java
+++ Intent/ActivityTest/app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -18,8 +18,9 @@ public class FirstActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse("http://www.baidu.com"));
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:10086"));
+
                 startActivity(intent);
 
             }
