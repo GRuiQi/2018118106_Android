21800275f18ee75fc54ff5292ec65a73b53dcb90
GRuiQi <2654344573@qq.com>
2020-11-17 08:33:10 +0800

    返回数据给上一个活动

--- Intent/ActivityTest/app/src/main/java/com/example/activitytest/FirstActivity.java
+++ Intent/ActivityTest/app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -1,10 +1,12 @@
 package com.example.activitytest;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -18,13 +20,26 @@ public class FirstActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String data = "Hello,SecondActivity";
-                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
-                intent.putExtra("extra_data",data);
 
-                startActivity(intent);
+                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+                startActivityForResult(intent,1);
 
             }
         });
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    String returnedData = data.getStringExtra("data_return");
+                    Log.d("FirstActivity", returnedData);
+                }
+                break;
+            default:
+        }
     }
 }
