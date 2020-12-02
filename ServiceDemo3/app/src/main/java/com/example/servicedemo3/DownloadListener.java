98695c8fb189283939eecd54adb963bf6cfe163d
GRuiQi <2654344573@qq.com>
2020-12-02 12:57:53 +0800

    完成服务下载案例

---
+++ ServiceDemo3/app/src/main/java/com/example/servicedemo3/DownloadListener.java
@@ -0,0 +1,15 @@
+package com.example.servicedemo3;
+
+public interface DownloadListener {
+
+    void onProgress(int progress);
+
+    void onSuccess();
+
+    void onFailed();
+
+    void onPaused();
+
+    void onCanceled();
+
+}
